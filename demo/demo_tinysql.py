88a0a5f121191c9284380646b214e4ea1e3b3043
Nicolai Waniek <rochus@rochus.net>
2025-05-15 14:03:20 +0200

    removed double shebang line

--- demo/demo_tinysql.py
+++ demo/demo_tinysql.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-#!/usr/bin/env python
-
 # be able to run this without actually installing snek
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
